529d576d965f3818bb9e93244a7a97b6d080f867
Travis Swicegood <development@domain51.com>
2012-03-17 16:13:32 -0500

    bump to dev version in prep for v0.4.1

--- django_ses/__init__.py
+++ django_ses/__init__.py
@@ -9,7 +9,7 @@ from time import sleep
 
 
 # When changing this, remember to change it in setup.py
-VERSION = (0, "4", 0)
+VERSION = (0, "4", "1alpha", 0)
 __version__ = '.'.join([str(x) for x in VERSION])
 __author__ = 'Harry Marr'
 __all__ = ('SESBackend',)

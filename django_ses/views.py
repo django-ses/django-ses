eecc27d5b1053da3327201667dd2ef91fa082636
Ian Lewis <IanMLewis@gmail.com>
2013-03-16 21:06:21 +0900

    Updated the bounce handler.
    Fixed the bounce handler to handle the correct notification format as
    received from SNS.

--- django_ses/views.py
+++ django_ses/views.py
@@ -1,3 +1,4 @@
+import urllib2
 import copy
 import logging
 from datetime import datetime
@@ -11,6 +12,7 @@ from boto.regioninfo import RegionInfo
 from boto.ses import SESConnection
 
 from django.http import HttpResponse, HttpResponseBadRequest
+from django.views.decorators.http import require_POST
 from django.core.cache import cache
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import render_to_response
@@ -156,6 +158,7 @@ def dashboard(request):
     cache.set(cache_key, response, 60 * 15)  # Cache for 15 minutes
     return response
 
+@require_POST
 def handle_bounce(request):
     """
     Handle a bounced email via an SNS webhook.
@@ -170,7 +173,19 @@ def handle_bounce(request):
 
     TODO: Verify the SNS message signature.
     See: http://docs.amazonwebservices.com/sns/latest/gsg/SendMessageToHttp.verify.signature.html
+
+
+    In addition to email bounce requests this endpoint also supports the SNS
+    subscription confirmation request. This request is sent to the SNS
+    subscription endpoint when the subscription is registered.
+    See: http://docs.aws.amazon.com/sns/latest/gsg/Subscribe.html
+
+    For the format of the SNS subscription confirmation request see this URL:
+    http://docs.aws.amazon.com/sns/latest/gsg/json-formats.html#http-subscription-confirmation-json
+    
     """
+    import pdb;pdb.set_trace()
+    
     # For Django >= 1.4 use request.body, otherwise
     # use the old request.raw_post_data
     if hasattr(request, 'body'):
@@ -179,70 +194,98 @@ def handle_bounce(request):
         raw_json = request.raw_post_data
 
     try:
-        hook_data = json.loads(raw_json)
+        notification = json.loads(raw_json)
     except ValueError, e:
         # TODO: What kind of response should be returned here?
         logger.warning('Recieved bounce with bad JSON: "%s"', e)
         return HttpResponseBadRequest()
 
-    mail_obj = hook_data.get('mail')
-    notification_type = hook_data.get('notificationType')
     
-    if not notification_type:
-        logger.warning('Recieved bounce with no notificationType.', extra={
-            'message': hook_data,
-        })
-        # TODO: What kind of response should be returned here?
-        return HttpResponseBadRequest()
 
-    if notification_type == 'Bounce':
-        # Bounce 
-        bounce_obj = hook_data.get('bounce', {})
-        
-        # Logging
-        feedback_id = bounce_obj.get('feedbackId')
-        bounce_type = bounce_obj.get('bounceType')
-        bounce_subtype = bounce_obj.get('bounceSubType')
-        logger.info(
-            'Recieved bounce notification: feedbackId: %s, bounceType: %s, bounceSubType: %s', 
-            feedback_id, bounce_type, bounce_subtype,
+    if notification.get('Type') == 'SubscriptionConfirmation':
+        # Process the subscription confirmation.
+
+        logger.info('Recieved subscription confirmation: TopicArn: %s', 
+            notification.get('TopicArn'),
             extra={
-                'message': hook_data,
+                'notification': notification,
             },
         )
 
-        signals.bounce_received.send(
-            sender=SESBounce,
-            mail_obj=mail_obj,
-            bounce_obj=bounce_obj,
-            raw_message=raw_json,
-        )
-        return HttpResponse()
-    elif notification_type == 'Complaint':
-        # Complaint
-        complaint_obj = hook_data.get('complaint', {})
+        # Get the subscribe url and hit the url to confirm the subscription.
+        subscribe_url = notification.get('SubscribeURL')
+        try:
+            urllib2.urlopen(subscribe_url).read()
+        except urllib2.URLError, e:
+            # Some kind of error occurred when confirming the request.
+            logger.error('Could not confirm subscription: "%s"', e,
+                extra={
+                    'notification': notification,
+                },
+                exc_info=True,
+            )
+    elif notification.get('Type') == 'Notification':
+        try:
+            message = json.loads(notification['Message'])
+        except ValueError, e:
+            # The message isn't JSON.
+            # Just ignore the notification.
+            logger.warning('Recieved bounce with bad JSON: "%s"', e, extra={
+                'notification': notification, 
+            })
+        else:
+            mail_obj = message.get('mail')
+            notification_type = message.get('notificationType')
+            
+            if notification_type == 'Bounce':
+                # Bounce 
+                bounce_obj = message.get('bounce', {})
+                
+                # Logging
+                feedback_id = bounce_obj.get('feedbackId')
+                bounce_type = bounce_obj.get('bounceType')
+                bounce_subtype = bounce_obj.get('bounceSubType')
+                logger.info(
+                    'Recieved bounce notification: feedbackId: %s, bounceType: %s, bounceSubType: %s', 
+                    feedback_id, bounce_type, bounce_subtype,
+                    extra={
+                        'notification': notification,
+                    },
+                )
 
-        # Logging
-        feedback_id = complaint_obj.get('feedbackId')
-        feedback_type = complaint_obj.get('complaintFeedbackType')
-        logger.info('Recieved complaint notification: feedbackId: %s, feedbackType: %s', 
-            feedback_id, feedback_type,
-            extra={
-                'message': hook_data,
-            },
-        )
+                signals.bounce_received.send(
+                    sender=SESBounce,
+                    mail_obj=mail_obj,
+                    bounce_obj=bounce_obj,
+                    raw_message=raw_json,
+                )
+            elif notification_type == 'Complaint':
+                # Complaint
+                complaint_obj = message.get('complaint', {})
 
-        signals.complaint_received.send(
-            sender=SESComplaint,
-            mail_obj=mail_obj,
-            complaint_obj=complaint_obj,
-            raw_message=raw_json,
-        )
+                # Logging
+                feedback_id = complaint_obj.get('feedbackId')
+                feedback_type = complaint_obj.get('complaintFeedbackType')
+                logger.info('Recieved complaint notification: feedbackId: %s, feedbackType: %s', 
+                    feedback_id, feedback_type,
+                    extra={
+                        'notification': notification,
+                    },
+                )
 
-        return HttpResponse()
-    else:
-        logger.warning('Recieved bounce with invalid notificationType: "%s"', notification_type, extra={
-            'message': hook_data,
-        })
-        # TODO: What kind of response should be returned here?
-        return HttpResponseBadRequest()
+                signals.complaint_received.send(
+                    sender=SESComplaint,
+                    mail_obj=mail_obj,
+                    complaint_obj=complaint_obj,
+                    raw_message=raw_json,
+                )
+            else:
+                # We received an unknown notification type. Just log and
+                # ignore it.
+                logger.warning("Recieved unknown notification", extra={
+                    'notification': notification,
+                })
+
+    # AWS will consider anything other than 200 to be an error response and
+    # resend the SNS request. We don't need that so we return 200 here.
+    return HttpResponse()

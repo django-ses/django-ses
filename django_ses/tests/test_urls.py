458ea3b10d31b8c05b1f230bac5d5180d74f0c04
Travis Swicegood <development@domain51.com>
2014-02-27 16:01:41 -0600

    Add support for Django 1.6

--- django_ses/tests/test_urls.py
+++ django_ses/tests/test_urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls.defaults import patterns, url
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    # Fall back to the old, pre-1.6 style
+    from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('django_ses.views',
     url(r'^dashboard/$', 'dashboard', name='django_ses_stats'),

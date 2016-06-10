14a7c79f453072936b1c50544db6a9cb8fb4e862
Eric Mills <eric.mills@rithmio.com>
2016-06-10 10:01:31 -0500

    fix url warnings for django 1.10

--- django_ses/tests/test_urls.py
+++ django_ses/tests/test_urls.py
@@ -4,8 +4,10 @@ except ImportError:
     # Fall back to the old, pre-1.6 style
     from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns('django_ses.views',
-    url(r'^dashboard/$', 'dashboard', name='django_ses_stats'),
-    url(r'^bounce/$', 'handle_bounce', name='django_ses_bounce'),
+from django_ses.views import dashboard, handle_bounce
+
+urlpatterns = [
+    url(r'^dashboard/$', dashboard, name='django_ses_stats'),
+    url(r'^bounce/$', handle_bounce, name='django_ses_bounce'),
     #url(r'^complaint/$', 'handle_complaint', name='django_ses_complaint'),
-)
+]

a8290b8661670c6093f271084223724aba24ca40
Tom Hendrikx <tom@whyscream.net>
2016-11-10 13:40:43 +0100

    Rename files so all test files are detected and ran under all django versions

--- django_ses/tests/test_backend.py
+++ django_ses/tests/test_backend.py
@@ -67,7 +67,7 @@ class FakeSESBackend(django_ses.SESBackend):
 class SESBackendTest(TestCase):
     def setUp(self):
         # TODO: Fix this -- this is going to cause side effects
-        django_settings.EMAIL_BACKEND = 'django_ses.tests.backend.FakeSESBackend'
+        django_settings.EMAIL_BACKEND = 'django_ses.tests.test_backend.FakeSESBackend'
         django_ses.SESConnection = FakeSESConnection
         self.outbox = FakeSESConnection.outbox
 
@@ -124,7 +124,7 @@ class SESBackendTest(TestCase):
 class SESBackendTestReturn(TestCase):
     def setUp(self):
         # TODO: Fix this -- this is going to cause side effects
-        django_settings.EMAIL_BACKEND = 'django_ses.tests.backend.FakeSESBackend'
+        django_settings.EMAIL_BACKEND = 'django_ses.tests.test_backend.FakeSESBackend'
         django_ses.SESConnection = FakeSESConnection
         self.outbox = FakeSESConnection.outbox
 

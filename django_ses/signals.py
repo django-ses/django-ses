c2e3e426793d9ac6882b7178152160f427ab6130
Ronny Vedrilla <ronny.vedrilla@ambient-innovation.com>
2020-08-07 09:53:36 +0200

    Removed `providing_args` for signals referring to deprecation warning in django 4.0

--- django_ses/signals.py
+++ django_ses/signals.py
@@ -1,7 +1,7 @@
 from django.dispatch import Signal
 
-bounce_received = Signal(providing_args=["mail_obj", "bounce_obj", "raw_message"])
+bounce_received = Signal()
 
-complaint_received = Signal(providing_args=["mail_obj", "complaint_obj", "raw_message"])
+complaint_received = Signal()
 
-delivery_received = Signal(providing_args=["mail_obj", "delivery_obj", "raw_message"])
+delivery_received = Signal()

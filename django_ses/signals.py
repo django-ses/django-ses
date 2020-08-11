cc1e41b79c74d4c40cd2e561059a46cf62bc55dd
Ronny Vedrilla <ronny.vedrilla@ambient-innovation.com>
2020-08-11 07:54:32 +0200

    Added comment about the removed variable for the signals

--- django_ses/signals.py
+++ django_ses/signals.py
@@ -1,7 +1,6 @@
 from django.dispatch import Signal
 
+# The following fields are used from the 3 signals below: mail_obj, bounce_obj, raw_message
 bounce_received = Signal()
-
 complaint_received = Signal()
-
 delivery_received = Signal()

6d149d564df219223f0dec105b922b798ac22d77
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-06-28 17:36:42 +0500

    Adding github actions. Old travis.org tests execution is not working any more. Added django3.0 and 3.1 testing.
    Adding github actions. Old travis.org tests execution is not working any more.
    Added django3.0 and 3.1 testing.
    
    Adding github actions. Old travis.org tests execution is not working any more.
    Added django3.0 and 3.1 testing.
    
    Adding github actions. Old travis.org tests execution is not working any more.
    Added django3.0 and 3.1 testing.
    
    Adding github actions. Old travis.org tests execution is not working any more.
    Added django3.0 and 3.1 testing.
    
    Adding github actions. Old travis.org tests execution is not working any more.
    Added django3.0 and 3.1 testing.
    
    Adding github actions. Old travis.org tests execution is not working any more.
    Added django3.0 and 3.1 testing.
    
    Adding github actions. Old travis.org tests execution is not working any more.
    Added django3.0 and 3.1 testing.

--- setup.py
+++ setup.py
@@ -128,12 +128,18 @@ CLASSIFIERS = [
     'Framework :: Django :: 2.0',
     'Framework :: Django :: 2.1',
     'Framework :: Django :: 2.2',
+    'Framework :: Django :: 3.0',
+    'Framework :: Django :: 3.1',
+    'Framework :: Django :: 3.2',
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
 ]
 
 setup(

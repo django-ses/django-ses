9fbfdf6b71e89d2aa1fd62ef9d4e5432ccb4b06b
Andrew Guenther <guenther.andrew.j@gmail.com>
2021-11-19 13:26:57 -0800

    Add default_auto_field to apps.py (#231)
    Fixes #230

--- django_ses/apps.py
+++ django_ses/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class DjangoSESConfig(AppConfig):
     name = 'django_ses'
     verbose_name = 'Django SES'
+
+    default_auto_field = 'django.db.models.AutoField'

9ac4c87367fdff9943e54c034f3cd41557ef4f0d
EricOuma <odhiamboeric0049@gmail.com>
2022-01-28 09:09:58 +0300

    add url for old dashboard view

--- django_ses/urls.py
+++ django_ses/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from django_ses.views import DashboardView
+from django_ses.views import DashboardView, dashboard
 
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='django_ses_stats'),
+    path('old/', dashboard, name='django_ses_stats_old'),
 ]

56836f090d7e2c251762788627541e8e739e614e
Ronny Vedrilla <ronny.vedrilla@ambient-innovation.com>
2022-02-23 09:11:46 +0100

    django 4.0 added to test matrix

--- example/urls.py
+++ example/urls.py
@@ -1,7 +1,6 @@
-from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import include
+from django.urls import include, re_path
 
 from django_ses.views import SESEventWebhookView
 from example import views
@@ -10,15 +9,15 @@ admin.autodiscover()
 
 urlpatterns = [
     '',
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    re_path(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    re_path(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', views.index, name='index'),
-    url(r'^send-email/$', views.send_email, name='send-email'),
-    url(r'^reporting/', include('django_ses.urls')),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^send-email/$', views.send_email, name='send-email'),
+    re_path(r'^reporting/', include('django_ses.urls')),
 
-    url(r'^bounce/', 'django_ses.views.handle_bounce', name='handle_bounce'),       # Deprecated, see SESEventWebhookView.
-    url(r'^event-webhook/', SESEventWebhookView.as_view(), name='event_webhook'),
+    re_path(r'^bounce/', 'django_ses.views.handle_bounce', name='handle_bounce'),       # Deprecated, see SESEventWebhookView.
+    re_path(r'^event-webhook/', SESEventWebhookView.as_view(), name='event_webhook'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from django_ses.views import DashboardView, handle_bounce, SESEventWebhookView
 
 urlpatterns = [
-    url(r'^dashboard/$', DashboardView.as_view(), name='django_ses_stats'),
-    url(r'^bounce/$', handle_bounce, name='django_ses_bounce'),
-    url(r'^event-webhook/$', SESEventWebhookView.as_view(), name='event_webhook'),
+    re_path(r'^dashboard/$', DashboardView.as_view(), name='django_ses_stats'),
+    re_path(r'^bounce/$', handle_bounce, name='django_ses_bounce'),
+    re_path(r'^event-webhook/$', SESEventWebhookView.as_view(), name='event_webhook'),
 ]

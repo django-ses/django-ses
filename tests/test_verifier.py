1d207dbe34e6b247b2413c3df8208d21e171bc28
Michael Lissner <mlissner@michaeljaylissner.com>
2022-03-28 23:57:15 -0700

    Update tests for cryptography and add valid messages

--- tests/test_verifier.py
+++ tests/test_verifier.py
@@ -10,47 +10,88 @@ except ImportError:
     requests = None
 
 try:
-    import M2Crypto
+    from cryptography import x509
 except ImportError:
-    M2Crypto = None
+    x509 = None
 
 from unittest import TestCase, skipIf
 
-from django_ses.utils import BounceMessageVerifier
+from django_ses.utils import BounceMessageVerifier, _CERT_CACHE
 
 
 class BounceMessageVerifierTest(TestCase):
     """
     Test for bounce message signature verification
     """
+
+    simple_msg = {
+        "Message": "Message text",
+        "MessageId": "Some ID",
+        "Subject": "Equity for all",
+        "Timestamp": "Yesterday",
+        "TopicArn": "arn:aws...",
+        "Type": "Notification",
+    }
+    # Any changes to this message will break the signature validity test.
+    valid_msg = {
+        "Type": "Notification",
+        "MessageId": "97903a0c-8818-5442-94b2-60b0c63d3da0",
+        "TopicArn": "arn:aws:sns:us-east-1:364852123998:test-email",
+        "Subject": "Amazon SES Email Event Notification",
+        "Message": '{"eventType":"Complaint","complaint":{"feedbackId":"0100017fd2f87864-7f070191-4eb1-49e3-a42e-7e996b718724-000000","complaintSubType":null,"complainedRecipients":[{"emailAddress":"complaint@simulator.amazonses.com"}],"timestamp":"2022-03-28T23:59:33.564Z","userAgent":"Amazon SES Mailbox Simulator","complaintFeedbackType":"abuse","arrivalDate":"2022-03-28T23:59:33.564Z"},"mail":{"timestamp":"2022-03-28T23:59:32.804Z","source":"jesus.islasf@alumno.buap.mx","sourceArn":"arn:aws:ses:us-east-1:364852123998:identity/jesus.islasf@alumno.buap.mx","sendingAccountId":"364852123998","messageId":"0100017fd2f875c4-c2929163-2767-49b1-8513-1a29e428e134-000000","destination":["complaint@simulator.amazonses.com"],"headersTruncated":false,"headers":[{"name":"From","value":"jesus.islasf@alumno.buap.mx"},{"name":"To","value":"complaint@simulator.amazonses.com"},{"name":"Subject","value":"Complaint Notification"},{"name":"MIME-Version","value":"1.0"},{"name":"Content-Type","value":"multipart/alternative;  boundary=\\"----=_Part_1160373_1543555432.1648511972808\\""}],"commonHeaders":{"from":["jesus.islasf@alumno.buap.mx"],"to":["complaint@simulator.amazonses.com"],"messageId":"0100017fd2f875c4-c2929163-2767-49b1-8513-1a29e428e134-000000","subject":"Complaint Notification"},"tags":{"ses:operation":["SendEmail"],"ses:configuration-set":["test-set"],"ses:source-ip":["189.203.131.80"],"ses:from-domain":["alumno.buap.mx"],"ses:caller-identity":["root"]}}}\n',
+        "Timestamp": "2022-03-28T23:59:33.713Z",
+        "SignatureVersion": "1",
+        "Signature": "MAnUSvI5C6S0GERh05oFiMtpZuGTW0J/6I/AaBinsZWK+Na8TwJsiNSUjShgZ8NItzQkjBnY1R1qT0wtPf6FfNXxGu3oQRaYsj1alz5NJyiuZwGlryX9LHuOoSJ7tGhrKA5yfYI1JPZsdUJKnkI3+UgKbIg7ml2FoSMU8s3HP3V/FOhvp6V5P6yt2okxABbw13WQPrzeUCZ9pRLgB3TnY59wsWM2SlynWEG/u/pFHyzuvkmtrGZtjZfITm7bfMnGy8FTOox4PfzCu4bysKlUbhc/yJ0fzI/+XsT2gKasXETzlmx6vd4qKgKWP5U9OJVh+Cx//npFDCBI2Tba8JK+Cg==",
+        "SigningCertURL": "https://sns.us-east-1.amazonaws.com/SimpleNotificationService-7ff5318490ec183fbaddaa2a969abfda.pem",
+        "UnsubscribeURL": "https://sns.us-east-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-east-1:364852123998:test-email:0fe0bffc-6470-4502-9414-d5e3d9fdd71e",
+    }
+
+    def tearDown(self):
+        # Reset the cache after each test
+        _CERT_CACHE.clear()
+
     @skipIf(requests is None, "requests is not installed")
-    @skipIf(M2Crypto is None, "M2Crypto is not installed")
+    @skipIf(x509 is None, "cryptography is not installed")
     def test_load_certificate(self):
         verifier = BounceMessageVerifier({})
-        with mock.patch.object(verifier, '_get_cert_url') as get_cert_url:
+        with mock.patch.object(verifier, "_get_cert_url") as get_cert_url:
             get_cert_url.return_value = "http://www.example.com/"
-            with mock.patch.object(requests, 'get') as request_get:
+            with mock.patch.object(requests, "get") as request_get:
                 request_get.return_value.status_code = 200
                 request_get.return_value.content = "Spam"
-                with mock.patch.object(M2Crypto.X509, 'load_cert_string') as load_cert_string:
-                    self.assertEqual(verifier.certificate, load_cert_string.return_value)
+                with mock.patch.object(
+                    x509, "load_pem_x509_certificate"
+                ) as load_cert_string:
+                    self.assertEqual(
+                        verifier.certificate, load_cert_string.return_value
+                    )
 
-    def test_is_verified(self):
-        verifier = BounceMessageVerifier({'Signature': base64.b64encode(b'Spam & Eggs')})
-        verifier._certificate = mock.Mock()
-        verify_final = verifier._certificate.get_pubkey.return_value.verify_final
-        verify_final.return_value = 1
-        with mock.patch.object(verifier, '_get_bytes_to_sign'):
-            self.assertTrue(verifier.is_verified())
+    @skipIf(requests is None, "requests is not installed")
+    @skipIf(x509 is None, "cryptography is not installed")
+    def test_valid_msg_validates(self):
+        """Does a valid message get validated properly?"""
+        verifier = BounceMessageVerifier(self.valid_msg)
+        self.assertTrue(verifier.is_verified())
 
-        verify_final.assert_called_once_with(b'Spam & Eggs')
+    @skipIf(requests is None, "requests is not installed")
+    @skipIf(x509 is None, "cryptography is not installed")
+    def test_invalid_message_fails(self):
+        """Does an invalid message fail validation?"""
+        verifier = BounceMessageVerifier(self.simple_msg)
+        self.assertFalse(verifier.is_verified())
 
-    def test_is_verified_bad_value(self):
-        verifier = BounceMessageVerifier({'Signature': base64.b64encode(b'Spam & Eggs')})
-        verifier._certificate = mock.Mock()
-        verifier._certificate.get_pubkey.return_value.verify_final.return_value = 0
-        with mock.patch.object(verifier, '_get_bytes_to_sign'):
-            self.assertFalse(verifier.is_verified())
+    @skipIf(requests is None, "requests is not installed")
+    @skipIf(x509 is None, "cryptography is not installed")
+    def test_cert_is_cached(self):
+        """Does the certificate get cached properly?"""
+        verifier = BounceMessageVerifier(self.valid_msg)
+        with mock.patch.object(requests, "get") as request_get, \
+                mock.patch.object(x509, "load_pem_x509_certificate"):
+            request_get.return_value.content = b"Spam"
+            request_get.return_value.status_code = 200
+            verifier.certificate
+            verifier.certificate
+            request_get.assert_called_once()
 
     def test_get_cert_url(self):
         """

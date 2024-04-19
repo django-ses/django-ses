9bf6317df872911371090a68fdf807b5e5fffde8
Ronny Vedrilla <ronny.vedrilla@ambient.digital>
2024-04-19 15:54:02 +0200

    Added "django-upgrade" linter and updated ruff version to 0.4.0

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,9 +1,9 @@
-from django.urls import re_path
+from django.urls import path
 
 from django_ses.views import DashboardView, SESEventWebhookView, handle_bounce
 
 urlpatterns = [
-    re_path(r'^dashboard/$', DashboardView.as_view(), name='django_ses_stats'),
-    re_path(r'^bounce/$', handle_bounce, name='django_ses_bounce'),
-    re_path(r'^event-webhook/$', SESEventWebhookView.as_view(), name='event_webhook'),
+    path('dashboard/', DashboardView.as_view(), name='django_ses_stats'),
+    path('bounce/', handle_bounce, name='django_ses_bounce'),
+    path('event-webhook/', SESEventWebhookView.as_view(), name='event_webhook'),
 ]

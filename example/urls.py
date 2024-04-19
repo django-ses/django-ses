fb5e1bcc49b753efee6d1303a7f8ec302595f8cf
Ronny V <ronny.vedrilla@ambient.digital>
2024-04-19 18:23:23 +0200

    Feature/django upgrade linter (#321)
    * Updated badges in Readme.rst
    
    * Added "django-upgrade" linter and updated ruff version to 0.4.0
    
    * CI changes

--- example/urls.py
+++ example/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import include, re_path
+from django.urls import include, path
 
 from django_ses.views import SESEventWebhookView
 from example import views
@@ -9,15 +9,15 @@ admin.autodiscover()
 
 urlpatterns = [
     '',
-    re_path(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    re_path(r'^admin/', include(admin.site.urls)),
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
+    path('admin/', include(admin.site.urls)),
 
-    re_path(r'^$', views.index, name='index'),
-    re_path(r'^send-email/$', views.send_email, name='send-email'),
-    re_path(r'^reporting/', include('django_ses.urls')),
+    path('', views.index, name='index'),
+    path('send-email/', views.send_email, name='send-email'),
+    path('reporting/', include('django_ses.urls')),
 
-    re_path(r'^bounce/', 'django_ses.views.handle_bounce', name='handle_bounce'),  # Deprecated, see SESEventWebhookView
-    re_path(r'^event-webhook/', SESEventWebhookView.as_view(), name='event_webhook'),
+    path('bounce/', 'django_ses.views.handle_bounce', name='handle_bounce'),  # Deprecated, see SESEventWebhookView
+    path('event-webhook/', SESEventWebhookView.as_view(), name='event_webhook'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()

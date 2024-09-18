4235c4e39ea126ceb90f291672194980bd839016
Alexander Nestorov <alexandernst@gmail.com>
2024-09-18 02:05:02 +0200

    Add more tests for unique constraint

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -23,7 +23,25 @@ class SignalsTestCase(TestCase):
         # ... but after enabling the feature...
         settings.AWS_SES_ADD_BOUNCE_TO_BLACKLIST = True
 
-        # ... it should inser the email in the blacklist
+        # ... it should insert the email in the blacklist
+        signals.bounce_handler(None, mail_obj, bounce_obj, notification)
+        count = models.BlacklistedEmail.objects.all().count()
+        self.assertEqual(count, 1)
+
+    def test_bounce_handler_twice(self):
+        count = models.BlacklistedEmail.objects.all().count()
+        self.assertEqual(count, 0)
+
+        mail_obj, bounce_obj, notification = get_mock_bounce("eventType")
+
+        settings.AWS_SES_ADD_BOUNCE_TO_BLACKLIST = True
+
+        # ... it should insert the email in the blacklist
+        signals.bounce_handler(None, mail_obj, bounce_obj, notification)
+        count = models.BlacklistedEmail.objects.all().count()
+        self.assertEqual(count, 1)
+
+        # ... it shouldn't insert the email in the blacklist again
         signals.bounce_handler(None, mail_obj, bounce_obj, notification)
         count = models.BlacklistedEmail.objects.all().count()
         self.assertEqual(count, 1)
@@ -43,7 +61,25 @@ class SignalsTestCase(TestCase):
         # ... but after enabling the feature...
         settings.AWS_SES_ADD_COMPLAINT_TO_BLACKLIST = True
 
-        # ... it should inser the email in the blacklist
+        # ... it should insert the email in the blacklist
+        signals.complaint_handler(None, mail_obj, complaint_obj, notification)
+        count = models.BlacklistedEmail.objects.all().count()
+        self.assertEqual(count, 1)
+
+    def test_complaint_handler_twice(self):
+        count = models.BlacklistedEmail.objects.all().count()
+        self.assertEqual(count, 0)
+
+        mail_obj, complaint_obj, notification = get_mock_complaint("eventType")
+
+        settings.AWS_SES_ADD_COMPLAINT_TO_BLACKLIST = True
+
+        # ... it should insert the email in the blacklist
+        signals.complaint_handler(None, mail_obj, complaint_obj, notification)
+        count = models.BlacklistedEmail.objects.all().count()
+        self.assertEqual(count, 1)
+
+        # ... it shouldn't insert the email in the blacklist again
         signals.complaint_handler(None, mail_obj, complaint_obj, notification)
         count = models.BlacklistedEmail.objects.all().count()
         self.assertEqual(count, 1)

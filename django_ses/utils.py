ecc0b0ff58b4b962b1c443276948ff68bc6ce591
Alex Iribarren <alex@almalinux.org>
2026-02-05 17:22:58 +0100

    Fix status check for bounced email addresses

--- django_ses/utils.py
+++ django_ses/utils.py
@@ -316,7 +316,7 @@ def get_permanent_bounced_emails_from_bounce_obj(bounce_obj: dict) -> list:
     https://docs.aws.amazon.com/ses/latest/DeveloperGuide/notification-contents.html#bounce-object
     """
     bounced_recipients = bounce_obj.get("bouncedRecipients", list())
-    return [br.get("emailAddress") for br in bounced_recipients if br.get("status").startswith("5")]
+    return [br.get("emailAddress") for br in bounced_recipients if br.get("status", "").startswith("5")]
 
 
 def get_emails_from_complaint_obj(complaint_obj: dict) -> list:

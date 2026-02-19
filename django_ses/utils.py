de5febe81f0e6e45d574999bea62873e8d4fed63
Alex Iribarren <alex@almalinux.org>
2026-02-19 18:08:13 +0100

    Update bounced email extraction for permanent bounces
    Refactor get_permanent_bounced_emails_from_bounce_obj to adapt to new format.

--- django_ses/utils.py
+++ django_ses/utils.py
@@ -315,8 +315,9 @@ def get_permanent_bounced_emails_from_bounce_obj(bounce_obj: dict) -> list:
     """Extracts permanent bounced email addresses only as a list of strings.
     https://docs.aws.amazon.com/ses/latest/DeveloperGuide/notification-contents.html#bounce-object
     """
-    bounced_recipients = bounce_obj.get("bouncedRecipients", list())
-    return [br.get("emailAddress") for br in bounced_recipients if br.get("status", "").startswith("5")]
+    if bounce_obj.get("bounceType") == "Permanent":
+        return [br.get("emailAddress") for br in bounce_obj.get("bouncedRecipients", list())]
+    return []
 
 
 def get_emails_from_complaint_obj(complaint_obj: dict) -> list:
